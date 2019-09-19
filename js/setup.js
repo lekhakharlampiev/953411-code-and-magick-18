e254ddc74ed47d3715c630f4f1f0d34c3f31b2b9
Aleksey <xarlamlexa1502@gmail.com>
2019-09-19 21:36:45 +0300

    Реализовал весь функционал согласно техническому заданию

--- js/setup.js
+++ js/setup.js
@@ -11,36 +11,34 @@ setupSimilar.classList.remove('hidden');
 
 var names = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surname = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coat = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var coat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyes = ['black', 'red', 'blue', 'yellow', 'green'];
 
 function getRandomElement(arr) {
   return arr[Math.floor(Math.random() * (0 - arr.length)) + arr.length];
 }
 
-var fullname = {
-  name: getRandomElement(names)
-};
-var coatColor = {
-  color: getRandomElement(coat)
-};
-var eyesColor = {
-  color: getRandomElement(eyes)
-};
+var similarWizards = [];
+for (var i = 0; i <= 4; i++) {
+  var wizard = {};
+  wizard.name = getRandomElement(names) + ' ' + getRandomElement(surname);
+  wizard.coatColor = getRandomElement(coat);
+  wizard.eyesColor = getRandomElement(eyes);
+  similarWizards.push(wizard);
+}
 
 function getSimilarWisardsList(arr) {
-
   var fragment = new DocumentFragment();
   for (var i = 0; i < arr.length; i++) {
     var cloneElement = similarItem.cloneNode(true);
     var wizardName = cloneElement.querySelector('.setup-similar-label');
     var wizardCoatColor = cloneElement.querySelector('.wizard-coat');
     var wizardEyesColor = cloneElement.querySelector('.wizard-eyes');
-    wizardName.textContent = arr[i];
-    wizardCoatColor.style.fill = 'red';
-    wizardEyesColor.style.fill = 'blue';
+    wizardName.textContent = arr[i].name;
+    wizardCoatColor.style.fill = arr[i].coatColor;
+    wizardEyesColor.style.fill = arr[i].eyesColor;
     fragment.prepend(cloneElement);
   }
   return fragment;
 }
-similarList.prepend(getSimilarWisardsList(names));
+similarList.prepend(getSimilarWisardsList(similarWizards));
